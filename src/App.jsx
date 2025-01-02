5833ff9699c8a801a4c90262fff3dcc46693d804
Ritesh <ritsrivastava@gmail.com>
2025-01-02 12:44:09 +0100

    add initial components and configuration for React and Vite setup

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { Nav } from './components/Nav';
+import CustomerReview from './sections/CustomerReview';
+import Footer from './sections/Footer';
+import { Hero } from './sections/Hero';
+import { PopularProduct } from './sections/PopularProduct';
+import { Services } from './sections/Services';
+import { Subscribe } from './sections/Subscribe';
+import SuperQuality from './sections/SuperQuality';
+
+const App = () => (
+  <main className='relative'>
+    {/* <Nav /> */}
+    <section className='xl:padding-1 wide:padding-r padding-b'>
+      <Hero />
+    </section>
+    <section></section>
+    <section className='padding'>
+      <PopularProduct />
+    </section>
+    <section className='padding'>
+      <SuperQuality />
+    </section>
+    <section className='padding-x py-10'>
+      <Services />
+    </section>
+    <section className='padding bg-pale-blue'>
+      <CustomerReview />
+    </section>
+    <section className='padding-x sm:py-23 py-14 w-full'>
+      <Subscribe />
+    </section>
+    <section className='padding-x padding-t pb-8 bg-black'>
+      <Footer />
+    </section>
+  </main>
+);
+
+export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import { HeaderLogo } from '../assets/images';
+
+const Nav = () => {
+  return (
+    <header>
+      <nav>
+        <img src={HeaderLogo} alt='' />
+        NIle
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
